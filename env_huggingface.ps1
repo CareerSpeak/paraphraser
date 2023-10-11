3455c461354ff89fc6bc689f465e24320615e939
Tanmay Kulkarni <91152390+Darshula@users.noreply.github.com>
2023-10-11 15:33:12 +0000

    Add files for pretrained branch

---
+++ env_huggingface.ps1
@@ -0,0 +1,3 @@
+setx /M HF_HUB_DISABLE_SYMLINKS_WARNING "true"
+setx /M HUGGINGFACE_HUB_CACHE           "$PWD/~/.cache/huggingface"
+setx /M HF_DATASETS_CACHE               "$PWD/~/.cache/huggingface"
